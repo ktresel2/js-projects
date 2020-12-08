f59fb2f0e0495ee08060eb1e9b4655038be4f756
ktresel2 <67020850+ktresel2@users.noreply.github.com>
2020-12-07 23:11:10 -0500

    Add files via upload

---
+++ fifty-projects/background-slider/script.js
@@ -0,0 +1,43 @@
+const body = document.body
+const slides = document.querySelectorAll('.slide')
+const leftArrow = document.getElementById('left')
+const rightArrow = document.getElementById('right')
+
+let activeSlide = 4
+
+rightArrow.addEventListener('click', () => {
+    activeSlide++
+
+    if (activeSlide > slides.length - 1) {
+        activeSlide = 0
+    }
+
+    setBgToBody()
+    setActiveSlide()
+
+})
+
+leftArrow.addEventListener('click', () => {
+    activeSlide--
+
+    if (activeSlide < 0) {
+        activeSlide = slides.length - 1
+    }
+
+    setBgToBody()
+    setActiveSlide()
+
+})
+
+setBgToBody()
+
+function setBgToBody() {
+    body.style.backgroundImage = slides[activeSlide].style.backgroundImage
+}
+
+function setActiveSlide() {
+    slides.forEach((slide) => slide.classList.remove('active'))
+
+    slides[activeSlide].classList.add('active')
+}
+
